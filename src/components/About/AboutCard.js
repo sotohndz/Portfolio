21995dc256a87fb82451a798674147d7846b4f08
soto821 <sotohernandez821@outlook.com>
2024-03-27 13:18:31 -0400

    Update resume

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Soto Hernandez </span>
-            from <span className="purple"> Stockton, CA.</span>
+            from <span className="purple"> New Port Richey, FL.</span>
             <br /> I am a final year student pursuing an Integrated MSc (IMSc)
             in Computer Science.
             <br />
